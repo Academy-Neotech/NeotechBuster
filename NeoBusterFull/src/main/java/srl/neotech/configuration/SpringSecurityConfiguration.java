5716232ce44d1c613db14efef26e64e7dd95415c
Max <academy@neotech.srl>
2022-05-13 18:59:54 +0200

    sicurezza e admilte

--- NeoBusterFull/src/main/java/srl/neotech/configuration/SpringSecurityConfiguration.java
+++ NeoBusterFull/src/main/java/srl/neotech/configuration/SpringSecurityConfiguration.java
@@ -3,45 +3,91 @@ package srl.neotech.configuration;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
-import srl.neotech.services.AuthenticationService;
 
+@SuppressWarnings("deprecation")
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter{
 
 	@Autowired
     DataSource dataSource;
+
+	private static final String usersQuery = "select username, password, 1 FROM users where username = ? and enabled=1";
+	private static final String rolesQuery = "select u.username, a.authority_desc FROM users u, authority a where u.authority_id=a.authority_id and u.username = ?";
 	
-	@Autowired
-	AuthenticationService authService;
+	@Bean
+	public static NoOpPasswordEncoder passwordEncoder() {
+		return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+	}
 	
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 		web.ignoring().antMatchers("/resources/**");
 	}
 	
+
 	
+	@Autowired
+	public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication().usersByUsernameQuery(usersQuery).dataSource(dataSource).authoritiesByUsernameQuery(rolesQuery).passwordEncoder(passwordEncoder());
+	}
+
+	
+	/*
+	@Override
+	protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+		  auth.inMemoryAuthentication()
+          .withUser("customer").password("{noop}customer").roles("CUSTOMER");
+	}
+	
+	   @Bean
+   	@Override
+   	public UserDetailsService userDetailsService() {
+   		UserDetails user =
+   			 User.withDefaultPasswordEncoder()
+   				.username("customer")
+   				.password("customer")
+   				.roles("CUSTOMER")
+   				.build();
+
+   		return new InMemoryUserDetailsManager(user);
+   	}
+	*/
         @Override
     	protected void configure(HttpSecurity http) throws Exception {
     		http  
     		.csrf().disable()
     		.authorizeRequests()
-    		.antMatchers("/").permitAll()
-    		.antMatchers("/homepage*").hasAnyRole("MANAGER", "CUSTOMER")
+    		.antMatchers("/static/**").permitAll()
+    		.antMatchers("/login").permitAll()
+    		.antMatchers("/blank").permitAll()
     		.antMatchers("/customer/**").hasAnyRole("CUSTOMER")
 			.antMatchers("/manager/**").hasAnyRole("MANAGER")
 			.anyRequest().authenticated()
-    		.and()
-    	      .formLogin()
-    	      .loginPage("/login").permitAll()
-    	      .and()
-    	      .logout().permitAll();
- 
+			.and()
+			.httpBasic()
+			.and()
+			.formLogin()
+		    .loginPage("/login")
+		    .defaultSuccessUrl("/perform_login");
     	}
+        
+  
+        @Autowired
+        public void configureGlobal(AuthenticationManagerBuilder auth)
+          throws Exception {
+            auth.jdbcAuthentication()
+              .dataSource(dataSource);
+        }
 }
