6d6202cef9e8aa1dea8773190143948883b3a809
Federico <f.luchetti@neotech.srl>
2022-05-13 19:55:23 +0200

    registration

---
+++ NeoBusterFull/src/main/java/srl/neotech/requestresponse/RegistrationRequest.java
@@ -0,0 +1,17 @@
+package srl.neotech.requestresponse;
+
+import srl.neotech.model.UserDetail;
+
+public class RegistrationRequest {
+
+private UserDetail registration;
+
+public UserDetail getRegistration() {
+	return registration;
+}
+
+public void setRegistration(UserDetail registration) {
+	this.registration = registration;
+}	
+	
+}
